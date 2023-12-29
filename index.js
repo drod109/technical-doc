9f08db259a8f9e9824a6ce9716bee4a7273322c0
Dwayne Rodrigues <dwayne.rodrigues@hotmail.ca>
2023-12-29 16:43:11 -0500

    Update nav click event to close menu

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   var menuIcon = document.querySelector(".menu-icon");
   var header = document.querySelector("header");
 
-  menuIcon.addEventListener("click", function () {
+  function toggleNav() {
     header.classList.toggle("active");
-  });
+  }
+
+  menuIcon.addEventListener("click", toggleNav);
+
+  document
+    .querySelectorAll("#navbar .menu-item a")
+    .forEach(function (menuItem) {
+      menuItem.addEventListener("click", toggleNav);
+    });
 
   document
     .getElementById("toggle-theme")
