093582ec67d96999bfe6f6be09f2175dd5d22a27
Dwayne Rodrigues <dwayne.rodrigues@hotmail.ca>
2023-12-29 14:25:50 -0500

    Added menu for for smaller screens.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-document.getElementById("toggle-theme").addEventListener("click", function () {
-  document.documentElement.classList.toggle("dark-theme");
+document.addEventListener("DOMContentLoaded", function () {
+  var menuIcon = document.querySelector(".menu-icon");
+  var header = document.querySelector("header");
+
+  menuIcon.addEventListener("click", function () {
+    header.classList.toggle("active");
+  });
+
+  document
+    .getElementById("toggle-theme")
+    .addEventListener("click", function () {
+      document.documentElement.classList.toggle("dark-theme");
+    });
 });
